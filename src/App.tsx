552b2e833127c8038feba600d025d26a889c06a4
Julio Campos Machado <32886080+juliocamposmachado@users.noreply.github.com>
2025-10-28 06:19:43 -0300

    Update book cover image source in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -86,7 +86,7 @@ function App() {
         >
           <div className="w-64 h-80 rounded-md overflow-hidden">
             <img
-              src="https://raw.githubusercontent.com/juliocamposmachado/O-Tempo-que-Mora-em-Mim/main/src/capa_livro_marcela_franca.jpg"
+              src="https://raw.githubusercontent.com/juliocamposmachado/O-Tempo-que-Mora-em-Mim/refs/heads/main/src/capa.jpg"
               alt="Capa do livro O Tempo que Mora em Mim"
               className="w-full h-full object-cover transition-transform duration-500 hover:scale-105"
             />
