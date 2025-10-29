02366cc49a94eb9eba2869b28ea8f3aac3e1f18f
Julio Campos Machado <32886080+juliocamposmachado@users.noreply.github.com>
2025-10-29 03:10:57 -0300

    Update image source URL in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -95,7 +95,7 @@ function App() {
         >
           <div className="w-64 h-100 rounded-md overflow-hidden">
             <img
-              src="https://www.flickr.com/photos/197989739@N03/54887772360/in/dateposted-public/lightbox/"
+              src="https://live.staticflickr.com/65535/54887772360_62d97f4a74_n.jpg"
               alt="Capa do livro O Tempo que Mora em Mim"
               className="w-full h-full object-cover transition-transform duration-500 hover:scale-105"
             />
